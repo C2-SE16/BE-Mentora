8ebce7dfd9d52d81203cb98a935e9884735e0eea
DatBui305 <buianhdat305@gmail.com>
2025-04-19 22:56:32 +0700

    feat/add-to-favoritelist

--- src/app.module.ts
+++ src/app.module.ts
@@ -16,6 +16,7 @@ import { LectureModule } from './common/modules/lecture.module';
 import { RedisModule } from './common/cache/redis.module';
 import { RedisExampleController } from './common/controllers/redis-example.controller';
 import { UploadImageModule } from './common/modules/upload-image.module';
+import { FavoriteModule } from 'src/common/modules/favorite.module';
 
 @Module({
   imports: [
@@ -36,6 +37,7 @@ import { UploadImageModule } from './common/modules/upload-image.module';
     RedisModule,
     UploadImageModule,
     RedisModule,
+    FavoriteModule,
   ],
   controllers: [ElasticsearchController, RedisExampleController],
   providers: [
