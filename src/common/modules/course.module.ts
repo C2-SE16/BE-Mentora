dac39c5c48a460a75a76fac18de78792b83d4ca8
Elliot Nguyen <nguyenduykiet72@gmail.com>
2025-05-24 20:09:25 +0700

    UD: add course popular, new and trending

--- src/common/modules/course.module.ts
+++ src/common/modules/course.module.ts
@@ -4,11 +4,13 @@ import { CourseService } from '../services/course.service';
 import { PrismaModule } from '../prisma/prisma.module';
 import { ElasticsearchModule } from './elasticsearch.module';
 import { VoucherService } from '../services/voucher.service';
+import { CourseDiscoverController } from '../controllers/course-discover.controller';
+import { CourseDiscoverService } from '../services/course-discover.service';
 
 @Module({
   imports: [PrismaModule, ElasticsearchModule],
-  controllers: [CourseController],
-  providers: [CourseService, VoucherService],
+  controllers: [CourseController, CourseDiscoverController],
+  providers: [CourseService, VoucherService, CourseDiscoverService],
   exports: [CourseService],
 })
 export class CourseModule {}
